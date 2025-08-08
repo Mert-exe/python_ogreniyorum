7b9c16ba5baf0b9d91b5af952f120560d8c84139
Mert-exe <mertevran1907@gmail.com>
2025-08-08 12:50:02 +0300

    Update arkaplansilici.py

--- alıstırmalar/arkaplansilici.py
+++ alıstırmalar/arkaplansilici.py
@@ -1,6 +1,6 @@
 from rembg import remove
 input_path = 'silmedeneme.jpg'
 output_path = 'output.png'
-with open(r"C:\python calismalar\alıstırmalar\silmedeneme.jpg", "rb") as input_file:  #dosya yolunu tam olarak belirtin
+with open(r"BURAYA DOSYA UZANTISI VEYA DOSYA İSMİ", "rb") as input_file:  #dosya yolunu tam olarak belirtin
     with open(output_path, "wb") as output_file:
         output_file.write(remove(input_file.read()))
